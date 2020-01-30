209068128eed85b5084359a180673303f58fbddb
Cruz Lambert Jorge z003h46b <jorge.cruz_lambert@siemens.com>
2020-01-30 17:00:17 +0000

    Change to sciprt

--- deployment-template.sh
+++ deployment-template.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-aws configure set aws_access_key_id $ECS_ACCESS_KEY
-aws configure set aws_secret_access_key $ECS_SECRET_KEY
-aws configure set region $ECS_REGION
-ecs-cli configure profile --profile-name ECSProfile --access-key $ECS_ACCESS_KEY --secret-key $ECS_SECRET_KEY
-ecs-cli configure --cluster $4 --default-launch-type FARGATE --region $ECS_REGION --config-name ECSClusterConfig
+aws configure set aws_access_key_id $6
+aws configure set aws_secret_access_key $7
+aws configure set region $8
+ecs-cli configure profile --profile-name ECSProfile --access-key $6 --secret-key $7
+ecs-cli configure --cluster $4 --default-launch-type FARGATE --region $8 --config-name ECSClusterConfig
 
 echo "$(ecs-cli --version)"
 
-services=$(aws ecs describe-services --services $1 --cluster $4 --region $ECS_REGION | jq '.services')
+services=$(aws ecs describe-services --services $1 --cluster $4 --region $8 | jq '.services')
 if [ "$services" = "[]" ] && [ $5 = "Yes" ]
 then
     echo "Deploying $1 with service-discovery-enabled"
